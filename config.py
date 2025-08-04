e7ca38209900dffe7e2c43b0ea803e2b8499d4ae
rtxsc <robotronix.uitm@gmail.com>
2025-08-05 00:26:31 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+
+# WiFi credentials. Set password to "" for open networks.
+WIFI_SSID = "enter here"
+WIFI_PASS = "enter here"
+
+# Fill in information from Blynk Device Info here.
+BLYNK_TEMPLATE_ID   = "enter here"
+BLYNK_TEMPLATE_NAME = "enter here"
+BLYNK_AUTH_TOKEN    = "enter here" 
+
+# Change the default Blynk server. Applicable for users with a white label plan.
+BLYNK_MQTT_BROKER   = "blynk.cloud"
+
+
